ba6d0f017acbd063a78667f5f34e53018109bc46
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-06-16 03:59:59 -0700

    Added tests

---
+++ __tests__/get.test.js
@@ -0,0 +1,84 @@
+const app = require('../index.js')
+const supertest = require('supertest')
+const request = supertest(app)
+
+it('tests the endpoints for the entire list of trials', async() => {
+    const trials = [{
+        id: 1,
+        name: 'The Akh Arah Amphitheatre (Hard)',
+        level: 50,
+        itemLevel: 80,
+        boss: 'Shiva',
+        partySize: 'Full Party, 8 man',
+        Tomestones: '15 Allagan Tomestones of Poetics',
+        loot: 'Shiva Card, Ice Tear',
+        expansion: 'A Realm Reborn'
+    },
+    {
+        id: 2,
+        name: 'The Final Steps of Faith',
+        level: 60,
+        itemLevel: 205,
+        boss: 'Nidhogg',
+        partySize: 'Full Party, 8 man',
+        Tomestones: '10 Allagan Tomestones of Poetics',
+        loot: 'Nidhogg Card',
+        patch: 'Heavensward'
+    },
+    {
+        id: 3,
+        name: 'Castrum Fluminis',
+        level: 70,
+        itemLevel: 335,
+        boss: 'Tsukuyomi',
+        partySize: 'Full Party, 8 man',
+        Tomestones: '12 Allagan Tomestones of Poetics',
+        loot: 'Tsukuyomi Card',
+        patch: 'Stormblood'
+    },
+    {
+        id: 4,
+        name: 'The Dancing Plague',
+        level: 73,
+        itemLevel: '',
+        boss: 'Titania',
+        partySize: 'Full Party, 8 man',
+        Tomestones: '',
+        loot: 'Titania Card',
+        patch: 'Shadowbringers'
+    },
+    {
+        id: 5,
+        name: 'The Crown of the Immaculate',
+        level: 79,
+        itemLevel: '',
+        boss: 'Innocence',
+        partySize: 'Full Party, 8 man',
+        Tomestones: '',
+        loot: 'Innocence Card',
+        patch: 'Shadowbringers'
+    },
+    ]
+const response = await request.get('/trials')
+
+expect(response.status).toBe(200)
+expect(response.body).toEqual(trials)
+})
+
+it('tests the endpoint for a single trial', async() => {
+    const trial = {
+        id: 3,
+        name: 'Castrum Fluminis',
+        level: 70,
+        itemLevel: 335,
+        boss: 'Tsukuyomi',
+        partySize: 'Full Party, 8 man',
+        Tomestones: '12 Allagan Tomestones of Poetics',
+        loot: 'Tsukuyomi Card',
+        patch: 'Stormblood'
+    }
+const response = await request.get('/trials/3')
+
+expect(response.status).toBe(200)
+expect(response.body).toEqual(trial)
+})
