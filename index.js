d96961f729742172631cf5d9f5018ba2e0eab5f6
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-06-16 02:04:31 -0700

    Added data.js and assigned endpoints to match the id of the data

--- index.js
+++ index.js
@@ -2,23 +2,23 @@
 // imports in node looks like this! (Changes every few years, constantly evolving)
 // syntax between node and javascript is one of the only real differences
 const express = require('express');
+const trials = require('./data.js')
 const app = express();
 // declaring what port to use
 const port = 3000;
 
 // our first ENDPOINT / ROUTE
 // Node will be told to listen for requests at the path: '/'
-app.get('/', (req, res) => {
+app.get('/trials', (req, res) => {
 // when it recieves a request at this path, it will respond with 'hello world'
-    res.send('Main page!');
+    res.json(trials);
 });
 
-app.get('/second-page', (req, res) => {
-    res.send('Second-page!');
-});
+app.get('/trials/:id', (req, res) => {
+    const id = Number(req.params.id);
 
-app.get('/third-page', (req, res) => {
-    res.send('Third page!');
+    const trial = trials.find((singleTrial) => singleTrial.id === id);
+    res.json(trial);
 });
 
 app.listen(port, () => {
